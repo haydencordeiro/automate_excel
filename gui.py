c335efa5faa5753553878457137edacdaa14d1ba
Grejo Joby <51746129+grejo-joby@users.noreply.github.com>
2020-04-11 17:01:42 +0530

    GUI Repush

--- gui.py
+++ gui.py
@@ -3,13 +3,11 @@ from Plot import PlotBar, PlotPie
 import pymsgbox
 from kap2 import Load_excel
 
-
 def Plot_Bar():
   try:
     PlotBar(e2.get())
   except:
     pymsgbox.alert('You have entered invalid column name!', 'Alert')
-
 
 def Plot_Pie():
   try:
@@ -17,10 +15,8 @@ def Plot_Pie():
   except:
     pymsgbox.alert('You have entered invalid column name!', 'Alert')
 
-
 def SendMessage():
   Load_excel(e2.get(), e1.get(), e3.get(), e4.get())
-
 
 master = tk.Tk()
 master.configure(bg='white')
@@ -40,7 +36,6 @@ e2.grid(row=1, column=1, padx=10, pady=10)
 e3.grid(row=2, column=1, padx=10, pady=10)
 e4.grid(row=3, column=1, padx=10, pady=10)
 
-
 for i in range(10):
     master.columnconfigure(i, weight=2)
     master.rowconfigure(i, weight=2)
@@ -48,20 +43,6 @@ for i in range(10):
 tk.Button(master,text='Send',command=SendMessage, bg='#F9D162', fg="#0A5688", font=("Helvetica", 10, "bold"), height=3, width=10, activebackground="#F3954F").grid(row=4,column=0,padx=10, pady=20)
 tk.Button(master, text='Plot Bar', command=Plot_Bar,bg='#F9D162', fg="#0A5688", font=("Helvetica", 10, "bold"), height=3, width=10, activebackground="#F3954F").grid(row=4, column=1, padx=10, pady=20)
 tk.Button(master, text='Plot Pie', command=Plot_Pie,bg='#F9D162', fg="#0A5688", font=("Helvetica", 10, "bold"), height=3, width=10, activebackground="#F3954F").grid(row=4, column=2, padx=10, pady=20)
-# tk.Button(master,
-#           text='Recognise',
-#           command=Recognise).grid(row=3,
-#                                     column=2,
-#                                     sticky=tk.W,
-#                                     pady=4)
-
-
-# tk.Button(master,
-#           text='Sort Attendance',
-#           command=Sort).grid(row=3,
-#                                     column=3,
-#                                     sticky=tk.W,
 
 master.mainloop()
-
 tk.mainloop()
